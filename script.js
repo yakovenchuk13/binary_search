ee7fae75e28e65aa6e92a2980138537cfe5b4a6e
Vlad <yakovenchuk20@gmail.com>
2019-09-24 22:26:03 +0300

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 6, 77, 88, 545, 665];
+
+const target = 6;
+
+let left = 0;
+let right = arr.length() - 1;
+
+
+while(left < right) {
+    const middle = left + (right - left) / 2;
+
+
+}
